faa19c67ec8b19ef40824b1c85afab734bbb87ee
gmkung <guangmian@gmail.com>
2025-11-06 19:11:41 +0800

    Change default port from 3000 to 3001

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import routes from "./routes/routes";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 
